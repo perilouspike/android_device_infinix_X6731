2eb1865b5f8b4d12c5fbb7b75efd23f959d52db3
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-10-10 16:02:04 +0530

    Update twrp_RMX3392.mk
    Fix build error.

--- twrp_RMX3392.mk
+++ twrp_RMX3392.mk
@@ -26,15 +26,13 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from ossi device
 $(call inherit-product, device/realme/RMX3392/device.mk)
 
-PRODUCT_DEVICE := ossi
-PRODUCT_NAME := omni_ossi
-PRODUCT_BRAND := oplus
-PRODUCT_MODEL := ossi
-PRODUCT_MANUFACTURER := oplus
+PRODUCT_DEVICE := RMX3392
+PRODUCT_NAME := twrp_RMX3392
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3392
+PRODUCT_MANUFACTURER := realme
 
-PRODUCT_GMS_CLIENTID_BASE := android-oplus
+PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_k6877v1_64_k419-user 12 SP1A.210812.016 1695199805380 release-keys"
-
-BUILD_FINGERPRINT := oplus/ossi/ossi:12/SP1A.210812.016/1695199805380:user/release-keys
