b53ed19a4a1769a55690c8fb32b342c265055c92
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-11-03 20:13:29 +0000

    Nuke flag 'BOARD_USES_RECOVERY_AS_BOOT := true'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ AB_OTA_PARTITIONS += \
     odm \
     vbmeta_vendor \
     vbmeta_system
-BOARD_USES_RECOVERY_AS_BOOT := true
+#BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture
 TARGET_ARCH := arm64
